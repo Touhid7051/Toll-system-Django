d5fbbc1af6e245f3814b95a1a2c5fa44ef5e1260
Touhid <touhidul15-7051@diu.edu.bd>
2020-12-07 17:37:47 +0600

    Reseting Password with Email(SMTP mailing)

--- CRUD_App/urls.py
+++ CRUD_App/urls.py
@@ -2,6 +2,8 @@ from django.urls import path
 from .views import student_admission, index, student_list, Student_details, student_update, Student_delete, \
     registerPage, loginPage , logutPage ,search
 
+from django.contrib.auth import views as auth_views #for changing password
+
 urlpatterns = [
     path('', index ,name='index'),
     path('admission_form/', student_admission ,name='student_admission'),
@@ -13,5 +15,16 @@ urlpatterns = [
     path('login/', loginPage , name='login'),
     path('logout/', logutPage , name='logout'),
     path('search/', search, name='search'),
+
+    #For reset password
+
+    path('reset_password/', auth_views.PasswordResetView.as_view(template_name='password_reset.html'), name="reset_password"),
+
+    path('reset_password_sent/', auth_views.PasswordResetDoneView.as_view(template_name='password_reset_sent.html'),name="password_reset_done"),
+
+path(
+    'reset/<uidb64>/<token>/', auth_views.PasswordResetConfirmView.as_view(template_name='password_reset_form.html'),name='password_reset_confirm',),
+
+    path('reset_password_complete/', auth_views.PasswordResetCompleteView.as_view(template_name='password_reset_done.html'),name="password_reset_complete"),
 
 ]
--- CRUD_Django/settings.py
+++ CRUD_Django/settings.py
@@ -135,3 +135,13 @@ MEDIA_URL = '/media/'
 
 MEDIA_ROOT = MEDIA_DIR
 
+#SMTP CONFIGURATION
+
+EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+
+EMAIL_HOST_USER = 'Email_ID'
+EMAIL_HOST_PASSWORD ='Password'
+
